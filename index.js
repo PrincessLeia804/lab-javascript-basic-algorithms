1edde13709549e94b42864f2a7bee2b318b3869b
Lea Model <lea.skywalker309@gmail.com>
2023-06-15 10:54:35 +0200

    Adjustment to fit the instructions

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Iteration 1: Names and Input
-const hacker1 = "Mark";
+let hacker1 = "Mark";
 console.log(`The driver's name is ${hacker1}`);
 
-const hacker2 = "David";
+let hacker2 = "David";
 console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
@@ -17,37 +17,58 @@ if(hacker1.length > hacker2.length) {
 
 
 // Iteration 3: Loops
+// 3.1.
+hacker1.toUpperCase();
+let nameSpaced = '';
+
 for (let i = 0; i < hacker1.length; i++) {
-    console.log(hacker1[i]);
+    nameSpaced += hacker1[i].toUpperCase() + " ";
 }
 
+console.log(nameSpaced.trim());
+
+//3.2
+let nameBackwards = '';
 for (let i = hacker2.length - 1; i >= 0; i--) {
-    console.log(hacker2[i]);
+    nameBackwards += hacker2[i];
 }
+console.log(nameBackwards);
 
 
-if(hacker1.charCodeAt(0) < hacker2.charCodeAt(0)) {
+//3.3
+hacker1.toUpperCase();
+hacker2.toUpperCase();
+
+if(hacker1.localeCompare(hacker2)) {
     console.log(`The driver's name goes first.`);
-} else if(hacker1.charCodeAt(0) > hacker2.charCodeAt(0)) {
+} else if(hacker2.localeCompare(hacker1)) {
     console.log(`Yo, the navigator goes first, definitely.`);
-} else if(hacker1 === hacker2){
+} else {
     console.log(`What?! You both have the same name?`);
-}else {
-    console.log("No order applicable");
 }
 
 
 
 
 // Bonus 1
+const longText = `this is a test
 
-const longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras congue vitae nunc at facilisis. Nulla porta libero ut malesuada condimentum. Sed ullamcorper ipsum orci, vitae finibus est malesuada euismod. In vehicula sem non egestas hendrerit. Nam interdum purus eget tellus porta commodo. Cras nec eleifend tellus. Maecenas id ullamcorper diam, quis venenatis massa. Morbi tincidunt a mauris ut viverra. Duis in ligula volutpat, tincidunt velit vitae, molestie tortor. Quisque facilisis nisi et sagittis hendrerit. Pellentesque venenatis felis tortor, in auctor elit eleifend tempus. Morbi aliquam dolor nec mauris tincidunt ultrices. Integer quis elit et urna vulputate sollicitudin consectetur eget dolor. Sed dictum semper tincidunt. Nam eget ligula pellentesque, volutpat lectus sed, pulvinar nisi. Curabitur vitae maximus orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse semper, nisl et gravida auctor, urna est tempus arcu, sed faucibus nulla mauris a nunc. Proin ornare risus non eros vulputate consectetur. Suspendisse pretium risus sed pulvinar rutrum. Maecenas ac felis tempor, blandit felis non, finibus mi. Etiam quis nibh at nunc gravida lobortis sit amet in urna. In odio nisi, interdum ut egestas quis, efficitur sit amet purus. Aenean auctor, eros at laoreet feugiat, leo metus porta metus, faucibus posuere erat nibh eu elit. Fusce eget turpis eget nunc imperdiet sollicitudin in quis tortor. Nulla pharetra ultricies tortor id porta. Integer id lectus ut neque lobortis aliquet vitae sed sem. Pellentesque vel diam eleifend, vehicula orci non, sagittis sem. Aliquam scelerisque id purus et consequat. Pellentesque ligula quam, pulvinar vel laoreet id, dignissim et tellus. Fusce eget semper turpis. Ut sollicitudin quam mi, vel ullamcorper dui facilisis eget. Ut varius nisl eu condimentum congue. Pellentesque porta felis eget nulla venenatis finibus. Donec efficitur lacus a dolor aliquet, et aliquam urna consectetur. Praesent pulvinar consequat sollicitudin. Maecenas feugiat maximus purus dapibus placerat. Etiam molestie est ut risus congue, nec imperdiet odio pharetra. Aliquam tincidunt tortor congue, scelerisque libero non, laoreet risus.";
+let's see if that works
 
-let wordCounter = 0;
+yes?`
+const longerText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pharetra, felis ac sagittis efficitur, nisl est ullamcorper urna, id hendrerit massa lorem eget tellus. In congue porta ante et porta. Vivamus bibendum viverra cursus. Suspendisse lacinia justo orci, sit amet faucibus massa condimentum ac. Morbi porta augue nec ipsum tristique, non sodales urna dapibus. Nunc tristique egestas massa, ut molestie mauris ullamcorper at. Integer iaculis accumsan augue, et dapibus dui congue a. Pellentesque eu vestibulum nisi.
+
+Sed eget convallis quam, vitae egestas purus. Vivamus bibendum tellus gravida efficitur porttitor. Aenean finibus convallis leo, id varius lorem laoreet id. Donec posuere eleifend massa eget elementum. Nullam eleifend volutpat neque, quis efficitur urna accumsan sed. Etiam bibendum, mauris varius tincidunt malesuada, lorem tellus maximus arcu, vitae imperdiet lacus magna vitae nunc. Aenean pulvinar risus sit amet vehicula tincidunt. Nunc posuere felis in erat venenatis, auctor facilisis est tristique. Aenean interdum ipsum sed elit sagittis aliquet. Nam a augue sollicitudin, porttitor erat ut, varius risus.
+
+Mauris ut dapibus felis. Phasellus eget elit lorem. Integer semper non mi quis dictum. Suspendisse a lacus lorem. Vestibulum eget interdum eros, et sollicitudin felis. Fusce sodales non mauris fermentum congue. Praesent hendrerit maximus massa, ut gravida lectus porta vel. Nunc ultrices vestibulum libero, in blandit nisi egestas quis.`;
+
+
+
+let wordCounter = 1;
 let specificWordCounter = 0;
 
 for (let i = 0; i < longText.length; i++) {
-    if(longText[i] === " ") {
+    if(longText[i] === " " || longText.substring(i, i+2) === "\n\n") {
         wordCounter += 1;
     }
 
